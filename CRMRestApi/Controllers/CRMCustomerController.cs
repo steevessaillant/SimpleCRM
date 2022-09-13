6bd9f4ac8f754eca020ffaed9a106c5f9b1448b7
SteevesSaillant <steevessaillant@gmail.com>
2022-09-13 10:37:45 -0400

    restructure solution

---
+++ CRMRestApi/Controllers/CRMCustomerController.cs
@@ -0,0 +1,44 @@
+﻿using CRMRepository;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace CRMRestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CRMCustomerController : ControllerBase
+    {
+        private readonly IRepository<Customer> repository;
+
+        public CRMCustomerController(IRepository<Customer> repository)
+        {
+            if (repository is null)
+            {
+                throw new ArgumentNullException(nameof(repository));
+            }
+
+            this.repository = repository;
+        }
+
+        [HttpPost]
+        public void Post(Customer customer)
+        {
+            this.repository.Add(customer);
+            this.repository.Save();
+        }
+
+        [HttpDelete]
+        public void Delete(Customer customer)
+        {
+            try
+            {
+                this.repository.Delete(this.repository.Get(customer));
+                this.repository.Save();
+            }
+            catch (Exception ex)
+            {
+                throw ex; 
+            }
+        }
+    }
+}
