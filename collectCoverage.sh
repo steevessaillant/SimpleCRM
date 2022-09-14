9760351bb07a6dd98bb3fa846eb1192c6f0d20d2
SteevesSaillant <steevessaillant@gmail.com>
2022-09-14 10:38:30 -0400

    configure

--- collectCoverage.sh
+++ collectCoverage.sh
@@ -3,7 +3,7 @@ mkdir -p .results
 rm -r -f .results/*
 dotnet test --collect:"XPlat Code Coverage" -r .results/log -l trx
 result=$?
-dotnet reportgenerator -reports:`find .results -name coverage.cobertura.xml` -targetdir:.results/reports
+#dotnet reportgenerator -reports:`find .results -name coverage.cobertura.xml` -targetdir:.results/reports
 if [ $result = 0 ]; then
     echo "All tests passed!"
 else
