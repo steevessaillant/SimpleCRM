a037bbabb6d16ab082f44750c23e08faa48dd775
SteevesSaillant <steevessaillant@gmail.com>
2022-09-20 14:28:46 -0400

    try with cd ../../../

--- CRMTests/Integration/CustomerHttpApiRestTest.cs
+++ CRMTests/Integration/CustomerHttpApiRestTest.cs
@@ -14,12 +14,33 @@ namespace CRMTests.Integration
         [Fact]
         public async void CustomerPostEndpointPingShouldRespondAlive()
         {
-            using var process = new Process();
-            string exePath = "../CRMRestApiV2/bin/Debug/net6.0/CRMRestApiV2.exe";
+            
 
+            using var process = new Process();
+            string exePath = "/home/runner/work/SimpleCRM/SimpleCRM//CRMRestApiV2/bin/Debug/net6.0/CRMRestApiV2.exe";
+            
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             {
                 exePath = "C:\\Users\\SteevesSaillant\\source\\repos\\XUnit_CSharp_Example\\CRMRestApiV2\\bin\\Debug\\net6.0\\CRMRestApiV2.exe";
+            }else
+            {
+                string command = "cd ../../../";
+                string result = "";
+                using (Process proc = new System.Diagnostics.Process())
+                {
+                    proc.StartInfo.FileName = "/bin/bash";
+                    proc.StartInfo.Arguments = "-c \" " + command + " \"";
+                    proc.StartInfo.UseShellExecute = false;
+                    proc.StartInfo.RedirectStandardOutput = true;
+                    proc.StartInfo.RedirectStandardError = true;
+                    proc.Start();
+
+                    result += proc.StandardOutput.ReadToEnd();
+                    result += proc.StandardError.ReadToEnd();
+
+                    proc.WaitForExit();
+                }
+                Console.Write(result);
             }
 
             process.StartInfo.FileName = exePath;
