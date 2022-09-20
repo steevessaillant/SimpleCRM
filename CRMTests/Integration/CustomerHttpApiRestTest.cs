376a1c8e7c8095e867f825d42401e98f7058ed64
SteevesSaillant <steevessaillant@gmail.com>
2022-09-20 14:06:22 -0400

    Add  RuntimeInformation OS Platform and set exe path according to OS.

--- CRMTests/Integration/CustomerHttpApiRestTest.cs
+++ CRMTests/Integration/CustomerHttpApiRestTest.cs
@@ -2,6 +2,7 @@
 using System.Diagnostics;
 using System.Net;
 using System.Net.Http;
+using System.Runtime.InteropServices;
 using Xunit;
 
 namespace CRMTests.Integration
@@ -13,16 +14,22 @@ namespace CRMTests.Integration
         [Fact]
         public async void CustomerPostEndpointPingShouldRespondAlive()
         {
-            using(var process = new Process())
-            {
-                process.StartInfo.FileName = "C:\\Users\\SteevesSaillant\\source\\repos\\XUnit_CSharp_Example\\CRMRestApiV2\\bin\\Debug\\net6.0\\CRMRestApiV2.exe";
-                process.Start(); ;
+            using var process = new Process();
+            string exePath = "/home/runner/work/SimpleCRM/SimpleCRM/CRMTests/bin/Debug/net6.0/CRMRestApiV2.exe";
 
-                client.BaseAddress = new Uri("http://localhost:5000");
-                var response = await client.GetAsync("/api/CRMCustomer/");
-                Assert.True(response.StatusCode.Equals(HttpStatusCode.OK));
-                process.Kill();
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                exePath = "C:\\Users\\SteevesSaillant\\source\\repos\\XUnit_CSharp_Example\\CRMRestApiV2\\bin\\Debug\\net6.0\\CRMRestApiV2.exe";
             }
+
+            process.StartInfo.FileName = exePath;
+            process.Start();
+
+
+            client.BaseAddress = new Uri("http://localhost:5000");
+            var response = await client.GetAsync("/api/CRMCustomer/");
+            Assert.True(response.StatusCode.Equals(HttpStatusCode.OK));
+            process.Kill();
         }
     }
 }
