b30026b7840ec329ed7009c7dab644982ec527c7
SteevesSaillant <steevessaillant@gmail.com>
2022-10-18 18:06:47 -0400

    validation des champs requis (web ui react)

--- simplecrmweb/cypress/component/customerFormTests.cy.js
+++ simplecrmweb/cypress/component/customerFormTests.cy.js
@@ -1,38 +1,43 @@
 import React from 'react'
 import { mount } from '@cypress/react18'
 import CustomerForm from '../../src/components/customerForm'
-describe('ComponentName.cy.js', () => {
+import dateFormat, { masks } from "dateformat";
+const now = new Date();
+
+describe('CustomerForm', () => {
     const id = '[data-cy=id]'
     const firstName = '[data-cy=firstName]'
     const lastName = '[data-cy=lastName]'
     const dob = '[data-cy=dob]'
+    const message = '[data-cy=message]'
 
     beforeEach(() => {
         cy.intercept('POST', 'http://localhost:5222/api/CRMCustomer', (request) => {
             request.reply({
-                statusCode : 200
+                statusCode: 200
             })
         }).as("postedCustomer")
     })
 
-    it("Customer form should mount with default values", () => {
+    it("should mount with default values", () => {
         mount(
             <CustomerForm />
         )
         cy.get('div').contains('Add new customer')
     })
-    it("Customer form should post data with default values (smoke test)", () => {
+
+    it("should post data with default values (smoke test)", () => {
 
         mount(
             <CustomerForm />
         )
             .then(() => {
-          
-                cy.get(firstName).type("CyTestFName")
-                cy.get(lastName).type("CyTestLName")
-                cy.get(dob).clear()
-                cy.get(dob).type('2000-01-01')
-                cy.get("form")
+                cy.get(id).type("CyTestId")
+                    .get(firstName).type("CyTestFName")
+                    .get(lastName).type("CyTestLName")
+                    .get(dob).clear()
+                    .get(dob).type('2000-01-01')
+                    .get("form")
                     .submit()
                     .then(() => {
                         cy.wait('@postedCustomer').then((interception) => {
@@ -42,4 +47,41 @@ describe('ComponentName.cy.js', () => {
             })
 
     })
+
+    it("should not post data with empty fields", () => {
+
+        mount(
+            <CustomerForm />
+        )
+            .then(() => {
+                cy.get("form")
+                    .submit()
+                    .then((e) => {
+                        cy.get(message).contains('Please fix the errors in the form')
+                    })
+            })
+
+    })
+
+    //TODO : installer momentjs pour validerr les dates
+    // it("should not post data if the customer is not 18 years old", () => {
+
+    //     var now = dateFormat(now, "isoDate");
+    //     mount(
+    //         <CustomerForm />
+    //     )
+    //         .then(() => {
+    //             cy.get(id).type("CyTestId")
+    //                 .get(firstName).type("CyTestFName")
+    //                 .get(lastName).type("CyTestLName")
+    //                 .get(dob).clear()
+    //                 .get(dob).type(now)
+    //                 .get("form")
+    //                 .submit()
+    //                 .then((e) => {
+    //                     cy.get(message).contains('Please fix the errors in the form')
+    //                 })
+    //         })
+
+    // })
 })
--- simplecrmweb/src/components/customerForm.js
+++ simplecrmweb/src/components/customerForm.js
@@ -1,5 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
+import SimpleReactValidator from 'simple-react-validator';
+
 function CustomerForm() {
+
+
+  const [, forceUpdate] = useState()
+  const validator = useRef(new SimpleReactValidator())
+
 
   const [id, setId] = useState("");
   const [firstName, setFirstName] = useState("");
@@ -9,66 +16,75 @@ function CustomerForm() {
 
   let handleSubmit = async (e) => {
     e.preventDefault();
-    fetch("http://localhost:5222/api/CRMCustomer", {
-      method: 'POST',
-      headers: {
-        'accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-       body: JSON.stringify({
-        id: id,
-        firstName: firstName,
-        lastName: lastName,
-        dateOfBirth: dateOfBirth
-          })
-    })
-      .then(response => {
-        if(response.ok === true){
-          setMessage("Customer, " + firstName + " " + lastName + " born on " + dateOfBirth +  " was posted successfully!")
-        }
+    if (!validator.current.allValid()) {
+      validator.current.showMessages();
+      setMessage("Please fix the errors in the form")
+      forceUpdate(1)      // rerender to show messages for the first time
+    } else {
+      fetch("http://localhost:5222/api/CRMCustomer", {
+        method: 'POST',
+        headers: {
+          'accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          id: id,
+          firstName: firstName,
+          lastName: lastName,
+          dateOfBirth: dateOfBirth
+        })
       })
-      .catch(error => {
-      // enter your logic for when there is an error (ex. error toast)
-       console.log(error)
-       setMessage(error);
-      });  
+        .then(response => {
+          if (response.ok === true) {
+            setMessage("Customer, " + firstName + " " + lastName + " born on " + dateOfBirth + " was posted successfully!")
+          }
+        })
+        .catch(error => {
+          // enter your logic for when there is an error (ex. error toast)
+          console.log(error)
+          setMessage(error);
+        });
+      }
+  }
 
-}
+  return (
+    <div className="App">
+      <form onSubmit={handleSubmit}>
+        <input data-cy="id" required
+          type="text"
+          value={id}
+          placeholder="Id"
+          onChange={(e) => setId(e.target.value)}
+        />
+        {validator.current.message('id', id, 'required|alpha_num', { className: 'text-danger' })}
+        <input data-cy="firstName" required
+          type="text"
+          value={firstName}
+          placeholder="First Name"
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+        {validator.current.message('firstName', firstName, 'required|alpha', { className: 'text-danger' })}
+        <input data-cy="lastName" required
+          type="text"
+          value={lastName}
+          placeholder="Last Name"
+          onChange={(e) => setLastName(e.target.value)}
+        />
+        {validator.current.message('lastName', lastName, 'required|alpha', { className: 'text-danger' })}
+        <input data-cy="dob" required
+          type="date"
+          value={dateOfBirth}
+          placeholder="DateOfBirth"
+          onChange={(e) => setDOB(e.target.value)}
+        />
+        {validator.current.message('dateOfBirth', dateOfBirth, 'required|alpha_num_dash', { className: 'text-danger' })}
+        <br />
+        <button type="submit">Add new customer</button>
 
-return (
-  <div className="App">
-    <form onSubmit={handleSubmit}>
-      <input data-cy="id" required="true"
-        type="text"
-        value={id}
-        placeholder="Id"
-        onChange={(e) => setId(e.target.value)}
-      />
-      <input data-cy="firstName"
-        type="text"
-        value={firstName}
-        placeholder="First Name"
-        onChange={(e) => setFirstName(e.target.value)}
-      />
-      <input data-cy="lastName"
-        type="text"
-        value={lastName}
-        placeholder="Last Name"
-        onChange={(e) => setLastName(e.target.value)}
-      />
-      <input data-cy="dob"
-        type="date"
-        value={dateOfBirth}
-        placeholder="DateOfBirth"
-        onChange={(e) => setDOB(e.target.value)}
-      />
-      <br/>
-      <button type="submit">Add new customer</button>
-
-      <div className="message">{message}</div> 
-    </form>
-  </div>
-);
+        <div className="message" data-cy="message">{message}</div>
+      </form>
+    </div>
+  );
 }
 
 export default CustomerForm;
