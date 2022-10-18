6ee5bbe2e4c3aed5b04bc82a102d3c2d2d99f7b4
SteevesSaillant <steevessaillant@gmail.com>
2022-10-18 14:44:44 -0400

    Commit REST API project, remove sensitive info + create react component for web client (no validation yet)

--- simplecrmweb/cypress/component/customerFormTests.cy.js
+++ simplecrmweb/cypress/component/customerFormTests.cy.js
@@ -2,35 +2,44 @@ import React from 'react'
 import { mount } from '@cypress/react18'
 import CustomerForm from '../../src/components/customerForm'
 describe('ComponentName.cy.js', () => {
-  const id = '[data-cy=id]'
-  const firstName = '[data-cy=firstName]'
-  const lastName = '[data-cy=lastName]'
-  const age = '[data-cy=age]'
+    const id = '[data-cy=id]'
+    const firstName = '[data-cy=firstName]'
+    const lastName = '[data-cy=lastName]'
+    const dob = '[data-cy=dob]'
 
-  it("Customer form should mount with default values", () => {
-    mount(
-      <CustomerForm minimumAge={18} />
-    )
-    cy.get('div').contains('Add new customer')
-    cy.get(age).should('have.value', '18')
-  })
-
-  it("Customer form should post data with default values (smoke test)", () => {
-    mount(
-      <CustomerForm minimumAge={18} />
-    )
-    .then(() => {
-      cy.get(id).type("CyTestId")
-      cy.get(firstName).type("CyTestFName")
-      cy.get(lastName).type("CyTestLName")
-      cy.get(age).clear()
-      cy.get(age).type(44)
-      cy.get("form")
-        .submit()
-    
-      
+    beforeEach(() => {
+        cy.intercept('POST', 'http://localhost:5222/api/CRMCustomer', (request) => {
+            request.reply({
+                statusCode : 200
+            })
+        }).as("postedCustomer")
     })
-   
-    
-  })
+
+    it("Customer form should mount with default values", () => {
+        mount(
+            <CustomerForm />
+        )
+        cy.get('div').contains('Add new customer')
+    })
+    it("Customer form should post data with default values (smoke test)", () => {
+
+        mount(
+            <CustomerForm />
+        )
+            .then(() => {
+          
+                cy.get(firstName).type("CyTestFName")
+                cy.get(lastName).type("CyTestLName")
+                cy.get(dob).clear()
+                cy.get(dob).type('2000-01-01')
+                cy.get("form")
+                    .submit()
+                    .then(() => {
+                        cy.wait('@postedCustomer').then((interception) => {
+                            expect(interception.response.statusCode).to.eq(200)
+                        })
+                    })
+            })
+
+    })
 })
--- simplecrmweb/src/components/customerForm.js
+++ simplecrmweb/src/components/customerForm.js
@@ -1,80 +1,74 @@
 import React, { useState } from "react";
-function CustomerForm(props) {
+function CustomerForm() {
 
-  const [Id, setId] = useState("");
-  const [FirstName, setFirstName] = useState("");
-  const [LastName, setLastName] = useState("");
-  const [DateOfBirth, setAge] = useState(props.minimumAge);
+  const [id, setId] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [dateOfBirth, setDOB] = useState("");
   const [message, setMessage] = useState("");
 
   let handleSubmit = async (e) => {
     e.preventDefault();
-    try {
-      let res = await fetch("http://localhost:5222/api/CRMCustomer", {
-        method: "POST",
-        mode: 'cors',
-        headers: {
-            'accept': 'application/json' ,
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin':  'http://localhost:8080',
-            'Access-Control-Allow-Methods': 'POST',
-            'Access-Control-Allow-Headers': 'Content-Type, Authorization'
-          },
-        body: JSON.stringify({
-          id:Id,
-          firstName: FirstName,
-          lastName: LastName,
-          age: DateOfBirth
-        }),
-      });
+    fetch("http://localhost:5222/api/CRMCustomer", {
+      method: 'POST',
+      headers: {
+        'accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+       body: JSON.stringify({
+        id: id,
+        firstName: firstName,
+        lastName: lastName,
+        dateOfBirth: dateOfBirth
+          })
+    })
+      .then(response => {
+        if(response.ok === true){
+          setMessage("Customer, " + firstName + " " + lastName + " born on " + dateOfBirth +  " was posted successfully!")
+        }
+      })
+      .catch(error => {
+      // enter your logic for when there is an error (ex. error toast)
+       console.log(error)
+       setMessage(error);
+      });  
 
-      if (res.status === 200) {
-        setFirstName("");
-        setLastName("");
-        setAge("");
-        setMessage("Customer with Id : " + Id + " created successfully");
-      } else {
-        setMessage("Some error occured" + resJson);
-      }
-    } catch (err) {
-      console.log(err);
-    }
-  };
+}
 
-  return (
-    <div className="App">
-      <form onSubmit={handleSubmit}>
-      <input data-cy="id"
-          type="text"
-          value={Id}
-          placeholder="Id"
-          onChange={(e) => setId(e.target.value)}
-        />
-        <input data-cy="firstName"
-          type="text" 
-          value={FirstName}
-          placeholder="First Name"
-          onChange={(e) => setFirstName(e.target.value)}
-        />
-        <input data-cy="lastName"
-          type="text"
-          value={LastName}
-          placeholder="Last Name"
-          onChange={(e) => setLastName(e.target.value)}
-        />
-        <input data-cy="age"
-          type="number"
-          value={DateOfBirth}
-          placeholder="DateOfBirth"
-          onChange={(e) => setAge(e.target.value)}
-        />
+return (
+  <div className="App">
+    <form onSubmit={handleSubmit}>
+      <input data-cy="id" required="true"
+        type="text"
+        value={id}
+        placeholder="Id"
+        onChange={(e) => setId(e.target.value)}
+      />
+      <input data-cy="firstName"
+        type="text"
+        value={firstName}
+        placeholder="First Name"
+        onChange={(e) => setFirstName(e.target.value)}
+      />
+      <input data-cy="lastName"
+        type="text"
+        value={lastName}
+        placeholder="Last Name"
+        onChange={(e) => setLastName(e.target.value)}
+      />
+      <input data-cy="dob"
+        type="date"
+        value={dateOfBirth}
+        placeholder="DateOfBirth"
+        onChange={(e) => setDOB(e.target.value)}
+      />
+      <br/>
+      <button type="submit">Add new customer</button>
 
-        <button type="submit">Add new customer</button>
-
-        <div className="message">{message ? <p>{message}</p> : null}</div>
-      </form>
-    </div>
-  );
+      <div className="message">{message}</div> 
+    </form>
+  </div>
+);
 }
 
 export default CustomerForm;
